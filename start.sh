6ad0fd7612f36080fdf9a81a2a91ca62a23bae9f
Oliver Filla <ocl@hotmail.de>
2018-04-30 15:50:23 +0200

    only define ARGV variable here

--- start.sh
+++ start.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-DEV="$1"
-ARGV=$@
-export DEV
+if [[ -z "$ARGV" ]]; then
+    # if ARGV is unset: set to arguments
+    ARGV=( $@ )
+fi
+
 export ARGV
 
 for file in $(ls ??_*.sh); do
     source $file
 done
+
+echo "all done"
