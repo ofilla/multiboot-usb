ddb85a5ff4b4ca5053b2bd5d80e95328c4cebb42
Oliver Filla <ocl@hotmail.de>
2018-05-10 21:53:37 +0200

    implemented unpacking of iso images

--- 40_unpack_images.sh
+++ 40_unpack_images.sh
@@ -1,4 +1,27 @@
 #!/bin/bash
 
-echo "$0: NOT IMPLEMENTED!" >&2
-exit 10
+iso_mountpoint=/tmp/iso
+mkdir -p $iso_mountpoint
+
+MOUNTPOINT=/media/of/BOOTUSB
+
+for iso in *.iso
+do
+    echo "found $iso"
+    
+    dirname=${iso%.iso}
+    dirname=$(echo $dirname | sed -e 's/[- ]/_/g' -e 's/[^A-Za-z0-9_]//g')
+    dest=$MOUNTPOINT/boot/$dirname
+
+    [[ -d $dest ]] && continue
+    mkdir $dest
+    
+    mount -t iso9660 -o ro $iso $iso_mountpoint
+    echo "  copying ..."
+    cp -Lr $iso_mountpoint/* $iso_mountpoint/.??* $MOUNTPOINT/boot/$dirname/
+    umount -lf $iso_mountpoint
+
+    echo " copied to $dirname"
+done
+
+rmdir $iso_mountpoint
