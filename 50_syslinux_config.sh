c84cad6e98a06e7479be50f549903ea78d219e94
Oliver Filla <ocl@hotmail.de>
2018-06-30 16:56:30 +0200

    fixed: memdisk reserves not enough RAM

--- 50_syslinux_config.sh
+++ 50_syslinux_config.sh
@@ -2,19 +2,11 @@
 
 source config
 
-####### config #######
-INCLUDEDIR="/boot/syslinux/config"
-ISODIR='/boot/iso'
-menufilename="load_isos.cfg"
-####### config #######
-
 CONFIGDIR="$MOUNTPOINT$INCLUDEDIR"
 ISOPATH="$MOUNTPOINT$ISODIR"
 menufile="$CONFIGDIR/$menufilename"
 
-##### mount
 mount ${DEV}1 $MOUNTPOINT
-
 
 
 ##### reset menufile
@@ -33,28 +25,29 @@ echo 'reset menufile'
 echo -e "INCLUDE $INCLUDEDIR/stdmenu.cfg" > $menufile
 
 
-
 ##### insert ISO entries
 echo "creating $menufile ..."
-for iso in $( ls $ISOPATH/*.iso $ISOPATH/*.ISO 2> /dev/null )
+for iso in $( ls $ISOPATH/*.iso 2> /dev/null )
 do
     filename="${iso##*/}"
     NAME=$(sed -e 's/[^A-Za-z0-9_.-]/-/g' <<< "$filename")
     NAME_HUMAN_READABLE=$(sed -e 's/.iso$//' -e 's/[_-]/ /g' -e 's/[^ ]*/\u&/g' <<< "$NAME" )
 
+    vmalloc=$(du -Lm $iso | cut -f1) # get size in MB
+    vmalloc="$(($vmalloc + 1))M"
     cat >> $menufile <<EOF
 
 LABEL $NAME
   MENU LABEL $NAME_HUMAN_READABLE
-  LINUX memdisk
+  LINUX memdisk vmalloc=$vmalloc
   INITRD $ISODIR/$filename
-  APPEND iso
+  APPEND iso raw
 EOF
 
     echo "  added iso entry for $NAME_HUMAN_READABLE"
 done
-echo "done creating $menufile"
 
+echo "done creating $menufile"
 
 
 ##### unmount
--- README.md
+++ README.md
@@ -13,13 +13,18 @@ Create a multiboot USB stick.
 ## tested with
 
 working:
+* **clonezilla-live-2.5.2-31-amd64**: CloneZilla Live
+* **dban.iso**: Darik's Boot and Nuke
+* **hirens-boot-cd.iso**: Hiren's Boot CD, version 15.2
 * **kali-linux-mini.iso**: Kali Linux Netinstall
 * **plpbt.iso**: Plop Boot Manager
+* **rescatux.iso**: RescATux
+* **riplinux.iso**: RIPLinuX 11.7
 * ~~**lubuntu-16.04.3-desktop-amd64.iso**: Lubuntu install disc~~ not working
 
 not working:
 * **hacking-live-1.0.iso** LiveCD from the book [Hacking - The Art of Exploitation](https://nostarch.com/hackingCD.htm)
-* **kali-linux-2018.2-amd64.iso** Kali Linux 
+* **KNOPPIX_2013-06-16-DE.iso**: Knoppix
 
 # see also
 
