6aaa926d917f42af8cc4cd2688f5ce22caee00be
Oliver Filla <ocl@hotmail.de>
2018-06-30 19:21:16 +0200

    fixed setting permissions, added verbosity

--- start.sh
+++ start.sh
@@ -9,10 +9,14 @@ source config
 export ARGV
 
 for file in $(ls ??_*.sh); do
+    echo "============ start: $file"
     source $file
+    echo "============ finished: $file"
 done
 
+mount ${DEV}1 $MOUNTPONT
 chmod -R o+r $MOUNTPOINT/*
 find $MOUNTPOINT -type d | xargs chmod o+x
+umount $MOUNTPOINT
 
 echo "all done"
