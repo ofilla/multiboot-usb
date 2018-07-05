e5d7c2aba5f6f89acbe068fb8099c999e1930881
Oliver Filla <ocl@hotmail.de>
2018-07-05 22:07:11 +0200

    added copy method: dd

---
+++ 40_dd_images.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+source config
+source functions_formatting.sh
+
+partnumber=2
+
+for iso in $(ls $dd_isodir)
+do
+    echo -n "copying $iso via dd ... "
+    dd if=$dd_isodir/$iso of=$DEV$partnumber bs=1M > /dev/null || abort "dd failed" 4
+    sync
+    echo done
+done
