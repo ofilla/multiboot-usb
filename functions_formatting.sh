396fa98adf7cf203a46f7021853d787df3061e03
Oliver Filla <ocl@hotmail.de>
2018-07-05 21:52:46 +0200

    extracted function

--- functions_formatting.sh
+++ functions_formatting.sh
@@ -1,22 +1,25 @@
 sector_size=$(gdisk -l $DEV | grep -P "Logical sector size: \d+ bytes" | cut -d' ' -f4)
 partnumber=1
 
-function create_partition() {
-    device=$1
-    size=$2
-    label=$3
-
+function assert_free_space_more_than() {
     # get free space on device
     sectors=$(gdisk -l $DEV | grep -P "Total free space is \d+ sectors" | cut -d' ' -f5)
     free=$(bc <<< "$sector_size * $sectors / 1024^2")
     
     # check if enough free space on device
-    if [[ $free -lt $size ]]; then
+    if [[ $free -lt $sectors ]]; then
 	echo "cannot create partition $partnumber with size $size" >&2
 	echo "E: not enough space on device" >&2
 	exit 4
     fi
 
+}
+
+function create_partition() {
+    device=$1
+    size=$2
+    label=$3
+    
     echo -e "creating partition $partnumber: \t ${SIZE}M \t for $label"
     # create new GPT and first partition
     gdisk $device > /dev/null <<EOF
@@ -30,5 +33,4 @@ w
 y
 EOF
     partnumber=$(($partnumber + 1))
-
 }
