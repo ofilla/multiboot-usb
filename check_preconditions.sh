13f73ca22439c3af0d85363ea0ee3804bf7f6bed
Oliver Filla <ocl@hotmail.de>
2018-07-06 10:51:08 +0200

    check if DEV is available

--- check_preconditions.sh
+++ check_preconditions.sh
@@ -3,9 +3,12 @@
 source config
 
 if [[ "$DEV" == "" ]]; then
-   echo "E: no destination device set"
+   echo "E: no destination device set" >&2
    exit 1
+elif [[ -z "$(ls $DEV 2> /dev/null)" ]]; then
+    echo "E: destination device '$DEV' is not available" >&2
+    exit 1
 elif [[ $UID != 0 ]]; then
-   echo "E: no root permissions"
+   echo "E: no root permissions" >&2
    exit 2
 fi
