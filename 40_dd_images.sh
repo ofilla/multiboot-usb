e3232d6c4643fd115e433cade4635b82e687c3c8
Oliver Filla <ocl@hotmail.de>
2018-07-07 22:06:53 +0200

    renamed file; recreate MBR after dd-ing images to make stick bootable again

---
+++ 40_dd_images.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+source config
+source check_preconditions.sh
+source functions_formatting.sh
+
+partnumber=2
+umount $DEV* 2> /dev/null
+
+for iso in $(ls $dd_isodir)
+do
+    echo "making $iso partition bootable ..."
+    isohybrid --partok $dd_isodir/$iso
+    echo -n "copying $iso via dd ... "
+    dd if=$dd_isodir/$iso of=$DEV$partnumber bs=1M &> /dev/null || abort "dd failed" 4
+    sync
+    echo done
+done
+
+umount $DEV* 2> /dev/null
+
+# re-create hybrid mbr
+gdisk $DEV > /dev/null <<EOF
+r
+h
+1
+y
+83
+y
+n
+w
+y
+EOF
+
+sync
