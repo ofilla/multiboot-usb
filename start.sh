bb7ee12bfdb3fe0d27ba62f0e6a9f99cffcdc7fb
Oliver Filla <ocl@hotmail.de>
2020-01-20 06:25:59 +0100

    Use fatattr instead of chmod (depending on file system)

--- start.sh
+++ start.sh
@@ -1,22 +1,30 @@
 #!/bin/bash
 
 if [[ -z "$ARGV" ]]; then
-    # if ARGV is unset: set to arguments
-    ARGV=( $@ )
+	# if ARGV is unset: set to arguments
+	ARGV=( $@ )
 fi
 
 source config
 export ARGV
 
 for file in $(ls ??_*.sh); do
-    echo "============ start: $file"
-    source $file
-    echo "============ finished: $file"
+	echo "============ start: $file"
+	source "$file"
+	echo "============ finished: $file"
 done
 
-mount ${DEV}1 $MOUNTPOINT
-chmod -R o+r $MOUNTPOINT/*
-find $MOUNTPOINT -type d | xargs chmod o+x
-umount $MOUNTPOINT
+if [[ "$FS" == *"fat" ]]; then
+	which fatattr &> /dev/null || alias fatattr='./fatattr'
+	echo "use fatattr instead of chmod"
+	CHMOD="fatattr +hsr -a"
+else
+	CHMOD="chmod o+r"
+fi
+
+mount "${DEV}1" "$MOUNTPOINT"
+find "$MOUNTPOINT" -type f -print0 | xargs -0 $CHMOD "$MOUNTPOINT/"*
+find "$MOUNTPOINT" -type d -print0 | xargs -0 chmod o+x
+umount "$MOUNTPOINT"
 
 echo "all done"
